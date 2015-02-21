15dc987e5eb8003fbac560b43fb845a516e70100
Olav Vatne <olavvatne@gmail.com>
2015-02-21 08:58:04 +0100

    inital commit, with prelim skeleton for EA

---
+++ individual.py
@@ -0,0 +1,7 @@
+
+class Individual(object):
+
+    def __init__(self, genotype, translator):
+        self.genotype
+        self.translator = translator
+        self.phenotype = None
---
+++ translator.py
@@ -0,0 +1,10 @@
+
+
+class AbstractTranslator(object):
+
+    def __init__(self):
+        pass
+
+class DefaultTranslator(AbstractTranslator):
+    def __init__(self):
+        pass
